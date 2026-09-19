5ad1d13e25231986bb29d5a179ac7445cf595235
maxgab201 <maxgab201@gmail.com>
2026-09-19 11:09:00 -0300

    fix(chat): parse accented highlight commands reliably

--- src/lib/chatActions.ts
+++ src/lib/chatActions.ts
@@ -7,7 +7,7 @@ export interface CreateHighlightsAction {
   page?: number;
 }
 
-const EXPLICIT_HIGHLIGHT = /^\s*(?:(?:por favor|please|s'il vous plaît|por favor|bitte|per favore)[,\s]+|(?:quiero que|podés|puedes|can you|could you|je veux que|peux-tu|pode|kannst du|puoi)\s+)?(?:subray(?:á|a|es|e|ar)|resalt(?:á|a|es|e|ar)|marc(?:á|a|as|ar)|destac(?:á|a|as|ar)|highlight|underline|mark|surligne|surligner|marque|marquer|destaque|destacar|sublinhe|sublinhar|markiere|markieren|evidenzia|evidenziare|sottolinea|sottolineare)\b/i;
+const EXPLICIT_HIGHLIGHT = /^\s*(?:(?:por favor|please|s'il vous plaît|bitte|per favore)[,\s]+|(?:quiero que|podés|puedes|can you|could you|je veux que|peux-tu|pode|kannst du|puoi)\s+)?(?:subray(?:á|a|es|e|ar)|resalt(?:á|a|es|e|ar)|marc(?:á|a|as|ar)|destac(?:á|a|as|ar)|highlight|underline|mark|surligne|surligner|marque|marquer|destaque|destacar|sublinhe|sublinhar|markiere|markieren|evidenzia|evidenziare|sottolinea|sottolineare)(?=\s|$|[.,;:!?¿¡])/i;
 
 const DOCUMENT_SCOPE = /(todo\s+el\s+documento|en\s+todo\s+el\s+documento|documento\s+completo|whole\s+document|entire\s+document|throughout\s+the\s+document|tout\s+le\s+document|document\s+entier|documento\s+inteiro|documento\s+todo|gesamte[nr]?\s+dokument|ganze[nr]?\s+dokument|intero\s+documento|tutto\s+il\s+documento)/i;
 
